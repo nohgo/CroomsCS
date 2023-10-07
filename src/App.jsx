1ad4967e19ceabcc41611f6b972dfc47c92118ca
nohgo <nohgoering@gmail.com>
2023-10-07 13:30:02 -0400

    committing changes through gh desktop

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Im renaming this to test it </h1>
+      <h1>Im renaming this to test it with gh desktop</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
