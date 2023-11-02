c369eec83cb72150cc91b3bd527bf591805c23ed
Arya4711 <armlk4711@gmail.com>
2023-11-02 15:29:52 -0400

    Buttons for portfolio carousel

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,9 +3,29 @@ import "../App.css/";
 
 function Portfolio() {
   return (
-    <div className="pageHeaders">
-      <h1 id="Portfolio">Portfolio</h1>
-    </div>
+    <>
+      <div className="pageHeaders">
+        <h1 id="Portfolio">Portfolio</h1>
+      </div>
+      <Button isLeft={true} />
+      <Button isLeft={false} />
+    </>
+  );
+}
+
+function Button({isLeft}) {
+  return (
+    <>
+      <div className={isLeft ? "left" : "right"}>
+        <button className="slideButton text-center">{isLeft ? "<" : ">"}</button>
+      </div>
+    </>
+  );
+}
+
+function Slides() {
+  return (
+    <img></img>
   );
 }
 
