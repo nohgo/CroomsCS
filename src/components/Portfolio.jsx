fcc20f1b77e09db2e84ba37576bba3de04a111c6
nohgo <60904645+nohgo@users.noreply.github.com>
2023-11-03 23:41:57 -0400

    made button switch photos :3

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,39 +1,56 @@
 import React from "react";
 import "../App.css/";
+import { useState } from "react";
 
 function Portfolio() {
+  const [count, setCount] = useState(1);
+
   return (
     <>
       <div className="pageHeaders">
         <h1 id="Portfolio">Portfolio</h1>
       </div>
       <div className="carasol">
-        <Button isLeft={true} />
-        <Slides />
-        <Button isLeft={false} />
+        <Button
+          isLeft={true}
+          onPress={() => count > 1 && setCount(count - 1)}
+        />
+        <Slides count={count} />
+        <Button
+          isLeft={false}
+          onPress={() => count < 2 && setCount(count + 1)}
+        />
       </div>
     </>
   );
 }
 
-function Button({ isLeft }) {
+function Button({ isLeft, onPress }) {
   return (
-    <>
-      <button
-        className={
-          isLeft
-            ? "slideButton text-center left"
-            : "slideButton text-center right"
-        }
-      >
-        {isLeft ? "<" : ">"}
-      </button>
-    </>
+    <button
+      onClick={onPress}
+      className={
+        isLeft
+          ? "slideButton text-center left"
+          : "slideButton text-center right"
+      }
+    >
+      {isLeft ? "<" : ">"}
+    </button>
   );
 }
 
-function Slides() {
-  return <img src="src/assets/MIQyt1kde0t.svg" className="centerImage"></img>;
+function Slides({ count }) {
+  return (
+    <>
+      {count == 1 && (
+        <img src="src/assets/MIQyt1kde0t.svg" className="centerImage"></img>
+      )}
+      {count == 2 && (
+        <img src="src/assets/react.svg" className="centerImage"></img>
+      )}
+    </>
+  );
 }
 
 export default Portfolio;
