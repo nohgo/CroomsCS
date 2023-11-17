e5a71988d68f6b34ba22e4baef814609de312b42
nohgo <60904645+nohgo@users.noreply.github.com>
2023-11-16 19:42:45 -0500

    added pins

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import '../App.css/';
+import React from "react";
+import "../App.css/";
 
 function Roadmap() {
   return (
-    <div className="roadmap"></div>
+    <div className="roadmap">
+      <img src="src/assets/reactPin.svg" className="react-pin"></img>
+      <img src="src/assets/reactPin.svg" className="game-pin"></img>
+      <img src="src/assets/reactPin.svg" className="mongo-pin"></img>
+    </div>
   );
 }
 
