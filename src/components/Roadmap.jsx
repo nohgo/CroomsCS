a0f934c36f71b850c35994be2dc13c3448a8be80
nohgo <60904645+nohgo@users.noreply.github.com>
2023-12-14 21:30:15 -0500

    Added appearing list to roadmap
    also I updated npm so if there are issues try to update

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import TimelineTitle from './TimelineTitle.jsx';
-import TimelineSubtext from './TimelineSubtext.jsx';
-import '../App.css/';
+import React from "react";
+import "../App.css/";
+import { useState } from "react";
 
 function Roadmap() {
-  const reactSub = ["point 1", "point 2", "point 3", "point 4"];
+  const [reactActive, setReact] = useState(false);
+  const [gameActive, setGame] = useState(false);
+  const [fullActive, setFull] = useState(false);
+
+  const reactSub = [
+    "Learn HTML, CSS, JS",
+    "Use React to create portfolio",
+    "Learn git for version control",
+  ];
+
+  const gameSub = [
+    "Continue to work with fundemental languages",
+    "Learn Node and expressJS",
+    "Integrate with react to create a full browser based game",
+  ];
+
+  const fullstackSub = [
+    "Learn MongoDB for database functionality",
+    "Use MERN to create a fullstack web app",
+    "Deploy app using webhosting site",
+  ];
   return (
     <>
       <div className="page">
@@ -12,16 +31,69 @@ function Roadmap() {
           <h1>Roadmap</h1>
         </div>
         <div className="timeline">
-          <TimelineTitle text="React" stage="react" />
-          <TimelineSubtext text={reactSub} stage="react-sub" />
-          <TimelineTitle text="Game" stage="game" />
-          <TimelineSubtext text={reactSub} stage="game-sub" />
-          <TimelineTitle text="Fullstack" stage="fullstack" />
-          <TimelineSubtext text={reactSub} stage="fullstack-sub" />
+          <TimelineTitle
+            onMouseEnter={() => setReact(true)}
+            onMouseLeave={() => setReact(false)}
+            text="React"
+            stage="react"
+          />
+          <TimelineSubtext
+            elementState={reactActive}
+            text={reactSub}
+            stage="react-sub"
+          />
+          <TimelineTitle
+            onMouseEnter={() => setGame(true)}
+            onMouseLeave={() => setGame(false)}
+            text="Game"
+            stage="game"
+          />
+          <TimelineSubtext
+            elementState={gameActive}
+            text={gameSub}
+            stage="game-sub"
+          />
+          <TimelineTitle
+            onMouseEnter={() => setFull(true)}
+            onMouseLeave={() => setFull(false)}
+            text="Fullstack"
+            stage="fullstack"
+          />
+          <TimelineSubtext
+            elementState={fullActive}
+            text={fullstackSub}
+            stage="fullstack-sub"
+          />
           <img src="src/assets/timeline.svg"></img>
         </div>
       </div>
     </>
+  );
+}
+
+function TimelineSubtext({ text, stage, elementState }) {
+  return (
+    <>
+      {elementState && (
+        <ul className={stage}>
+          {text.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+function TimelineTitle({ text, stage, onMouseEnter, onMouseLeave }) {
+  return (
+    <h2
+      className={stage}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {text}
+    </h2>
   );
 }
 
--- src/components/TimelineTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function TimelineTitle({text, stage}) {
-  return (
-    <h2 className={stage}>{text}</h2>
-  );
-}
-
-export default TimelineTitle;
